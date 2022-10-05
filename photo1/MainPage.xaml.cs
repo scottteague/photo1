181558cc44353e96e263d5917bed133e96736cbb
Scott Teague <scott.teague@jlgray.com>
2022-10-05 12:00:46 -0600

    Initial commit

---
+++ photo1/MainPage.xaml.cs
@@ -0,0 +1,25 @@
+﻿namespace photo1;
+
+public partial class MainPage : ContentPage
+{
+	public MainPage()
+	{
+        InitializeComponent();
+		img1.Source = "dotnet_bot.png";
+	}
+
+	private async void OnCounterClicked(object sender, EventArgs e)
+	{
+		try
+		{
+            var newImg = await MediaPicker.CapturePhotoAsync(new MediaPickerOptions { Title = "Take the shot" });
+            //var newImg = await MediaPicker.CapturePhotoAsync(null);
+            img1.Source = newImg?.FullPath;
+        }
+		catch 
+		{
+			img1.Source = "dotnet_bot.png";
+		}
+	}
+}
+
---
+++ photo1/Platforms/MacCatalyst/AppDelegate.cs
@@ -0,0 +1,9 @@
+﻿using Foundation;
+
+namespace photo1;
+
+[Register("AppDelegate")]
+public class AppDelegate : MauiUIApplicationDelegate
+{
+	protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+}
